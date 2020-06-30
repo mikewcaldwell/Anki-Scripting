ab1f1ee1325c4af972cd64641b8e5313ec574907
Mike Caldwell <mike-caldwell@hotmail.com>
2020-07-01 05:46:53 +0900

    Comments and minor changes.

--- import_cards.py
+++ import_cards.py
@@ -161,9 +161,9 @@ def import_sentences(sentences, deck):
                 f'[sound:{sentence.get_audio_file_name()}]', 
                 grammar_field]
         )
-        deck.add_note(aNote) 
+        deck.add_note(aNote)
 
-def create_chinese_deck(deck_name, url, media_folder):
+def create_deck(deck_name, url, media_folder):
     deck = genanki.Deck(
         random.randrange(1 << 30, 1 << 31),
         deck_name)
@@ -174,31 +174,44 @@ def create_chinese_deck(deck_name, url, media_folder):
     import_words(words, deck)
     return deck, words
 
-def create_anki_package(media_folder, output, level):
-    noun_deck, nouns = create_chinese_deck(
+def create_chugokugo_anki_package(media_folder, output_dir, level):
+    """
+    Creates an anki package for chugokugo-script website based on level passed in.
+    These cards are for Japanese speakers learning Chinese.
+
+    media_folder - Folder to download media files.
+    output_dir - Where to create the package.
+    level - Level of Chinese flashcards (1,2, or 3, 3 being the hardest.)
+    """
+    if level not in [1,2,3]:
+        raise ValueError("level needs to be 1, 2 or 3")
+    noun_deck, nouns = create_deck(
         f'中国語単語 レベル{level}::1. 名詞・代詞・量詞', 
         f'http://chugokugo-script.net/tango/level{level}/meishi.html',
         media_folder)
-    verb_deck, verbs = create_chinese_deck(
+    verb_deck, verbs = create_deck(
         f'中国語単語 レベル{level}::2. 動詞・助動詞・助詞', 
         f'http://chugokugo-script.net/tango/level{level}/doushi.html',
         media_folder)
-    adj_deck, adjectives = create_chinese_deck(
+    adj_deck, adjectives = create_deck(
         f'中国語単語 レベル{level}::3. 形容詞・副詞・その他', 
         f'http://chugokugo-script.net/tango/level{level}/keiyoushi.html',
         media_folder)
     
     media_files = []
     for word in (nouns + verbs + adjectives):
-        media_files += word.get_audio_files()
+        media_files += word.get_audio_file_paths()
     
+    #To use jquery on AnkiDroid, need to include jquery. 
+    #To do this need to prefix the file with _script, '_' meaning Anki won't try to delete the file.
     jquery_file = parse_site.download_file(
         'https://code.jquery.com/jquery-3.5.1.min.js', 
         media_folder, 
         filename = '_script.jquery-3.5.1.min.js')
     media_files.append(jquery_file)
+
     my_package = genanki.Package([noun_deck, verb_deck, adj_deck], media_files)
-    output_path = Path(output) / f'Chinese_Level_{level}.apkg'
+    output_path = Path(output_dir) / f'Chinese_Level_{level}.apkg'
     my_package.write_to_file(output_path)
 
 if __name__ == "__main__":
@@ -208,4 +221,4 @@ if __name__ == "__main__":
     parser.add_argument('level', type=int, choices=[1,2,3])
     args = parser.parse_args()
     
-    create_anki_package(args.media_folder, args.output, args.level)
+    create_chugokugo_anki_package(args.media_folder, args.output, args.level)
--- parse_site.py
+++ parse_site.py
@@ -18,14 +18,14 @@ class WordInfo:
         example_sentences, 
         additional_info, 
         picture):
-        self.word = word
-        self.translation = translation
-        self.pronunciation = pronunciation
-        self.definition = definition
-        self.audio = audio
-        self.example_sentences = example_sentences
-        self.additional_info = additional_info
-        self.picture = picture
+        self.word = word or ''
+        self.translation = translation or ''
+        self.pronunciation = pronunciation or ''
+        self.definition = definition or ''
+        self.audio = audio or ''
+        self.example_sentences = example_sentences or ''
+        self.additional_info = additional_info or ''
+        self.picture = picture or ''
 
     def __str__(self):
         sentence_string = ''
@@ -40,25 +40,24 @@ class WordInfo:
     Examples:\n {sentence_string}'''
 
     def get_audio_file_name(self):
+        #Return just the name (ie: file.mp3) for the audio of this word.
         return Path(self.audio).name
     
-    def get_audio_files(self):
+    def get_audio_file_paths(self):
+        #Returns an array of all audio files for this word (including example sentences).
         audio_file_names = []
-        if Path(self.audio).exists():
-            audio_file_names.append(self.audio)
+        audio_file_names.append(self.audio)
         for sentence in self.example_sentences:
-            if Path(sentence.audio).exists():
-                audio_file_names.append(sentence.audio)
+            audio_file_names.append(sentence.audio)
         return audio_file_names
-        
 
 class SentenceInfo:
     def __init__(self, sentence, translation, pronunciation, audio, grammar_used):
-        self.sentence = sentence
-        self.translation = translation
-        self.pronunciation = pronunciation
-        self.audio = audio
-        self.grammar_used = grammar_used
+        self.sentence = sentence or ''
+        self.translation = translation or ''
+        self.pronunciation = pronunciation or ''
+        self.audio = audio or ''
+        self.grammar_used = grammar_used or ''
     
     def __str__(self):
         grammar_string = ''
@@ -71,16 +70,18 @@ class SentenceInfo:
             Grammar:\n {grammar_string}'''
     
     def get_audio_file_name(self):
+        #Return the name (ie: file1.mp3) for the audio file for this sentence.
         return Path(self.audio).name
 
 class GrammarInfo:
     def __init__(self, point, explanation):
-        self.point = point
-        self.explanation = explanation
+        self.point = point or ''
+        self.explanation = explanation or ''
     def __str__(self):
         return f'               Grammar Point: {self.point}, Explanation: {self.explanation}'
 
 def __parse_grammars(grammar_set):
+    #take the soup representing grammar information and parse out the contents
     grammar_used=[]
     for grammar_html in grammar_set:
         grammar_item = grammar_html.summary.string or ''
@@ -91,12 +92,13 @@ def __parse_grammars(grammar_set):
     return grammar_used
 
 def __parse_sentences(sentence_set):
+    #take the soup representing an example sentence and parse out the contents
     example_sentences=[]
     for sentence_html in sentence_set:
         sentence = sentence_html.find(class_='divBunruiExC').string
         if sentence in [None, '']:
             continue
-        translation = sentence_html.find(class_='divBunruiExN').string or ''
+        translation = sentence_html.find(class_='divBunruiExN').string
         pronunciation = sentence_html.find(class_='divBunruiExP').string
         audio = sentence_html.find(class_='divBunruiExA').audio.source['src']
         grammar_used = __parse_grammars(sentence_html.find_all(class_='detailsExBunpou'))
@@ -104,21 +106,24 @@ def __parse_sentences(sentence_set):
     return example_sentences
 
 def __parse_word(word_html):
+    #take the soup representing word and parse out the contents
     word = word_html.find(class_='divBunruiC').string
     translation = word_html.find(class_='divBunruiN').string
     pronunciation = word_html.find(class_='divBunruiP').string
     audio = word_html.find(class_='divBunruiA').audio.source['src']
     example_sentences = __parse_sentences(word_html.find_all(class_='divBunruiExMain'))
-    word_info = WordInfo(word,translation,'',pronunciation,audio,example_sentences,'','')
+    word_info = WordInfo(word, translation, None, pronunciation, audio,example_sentences, None, None)
     return word_info
 
 def __fetch_text(url):
+    #return text for the website.
     page = requests.get(url)
     #need to enforce utf-8 or else encoding is messed up
     page.encoding = 'utf-8'
     return page.text
 
 def __parse_text(text):
+    #parse soup for the website, returning an array of all the word information
     soup = BeautifulSoup(text, 'html.parser')
     word_set = soup.find_all('div', { 'class': 'divBunruiRight'})
     words = []
@@ -128,7 +133,12 @@ def __parse_text(text):
     return words
 
 def __parse_site(url, use_cache):
+    #Read a webpage from online and parse out the information. 
+    #If use_cache is True, we will save the file into a cache folder.
     #this page doesn't change very often if at all, so just cache it
+    #cache it under a subfolder:
+    # 'http://chugokugo-script.net/tango/level1/keiyoushi.html'
+    #     -> ./cache/level1/keiyoushi.html
     if use_cache:
         parts = url.split('/')
         filename = parts.pop()
@@ -136,7 +146,7 @@ def __parse_site(url, use_cache):
 
         cache_dir  = Path('./cache') / subpath
         cache_dir.mkdir(parents=True, exist_ok=True)
-          
+        
         local_path = cache_dir / filename
         if not local_path.exists():
             text = __fetch_text(url)
@@ -147,30 +157,41 @@ def __parse_site(url, use_cache):
         text = __fetch_text(url)
     return __parse_text(text)
 
-def __parse_file(file):
-    local_path = Path(file)
-    text = local_path.read_text()
-    return __parse_text(text)
+def __download_audio_files(words, base_url, media_folder):
+    """Download audio files for the words and their sentence(s), then update to the local path.
+    """
+    for word in words:
+        word.audio = download_file(urljoin(base_url, word.audio), media_folder) 
+        for sentence in word.example_sentences:
+            sentence.audio = download_file(urljoin(base_url, sentence.audio), media_folder)
 
-def download_file(url, local_path, filename = None):
-    #make a path that looks like: http://chugokugo-script.net/tango/level1/../audio/人.mp3
+def download_file(url, directory, filename = None):
+    """
+    Given a path to the url, download it the given directory.
+    Defaults to the filename specified in the url, but can be overwritten.
+
+    url - URL to the file to download.
+    directory - Local directory to download the file too.
+    filename - The name to give the downloaded file.
+    """
     if not filename:
         filename = url.split('/').pop()
     
-    local_path = Path(local_path) / filename
+    local_path = Path(directory) / filename
 
     if not local_path.exists():
         content = requests.get(url).content          
         local_path.write_bytes(content)
     return str(local_path)
 
-def __download_audio_files(words, base_url, media_folder):
-    for word in words:
-        word.audio = download_file(urljoin(base_url, word.audio), media_folder) 
-        for sentence in word.example_sentences:
-            sentence.audio = download_file(urljoin(base_url, sentence.audio), media_folder)
 
 def parse(url, use_cache, media_folder):
+    """
+    Take a url, parse it, and download any referenced media files (mp3 in this case) to the media folder.
+    url - url to parse
+    use_cache - If true, check the cache for the file first.
+    media_folder - Where to download files to.
+    """
     words = __parse_site(url, use_cache)
     if media_folder is None:
         return words
